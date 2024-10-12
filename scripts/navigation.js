7e921509a978dcf4454142a77bd1656c64f3a52b
Sergey Babenko <sergj.babenko@gmail.com>
2024-10-12 14:04:46 +0300

    fix

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,4 +1,4 @@
-document.addEventListener('load', function() {
+document.addEventListener('DOMContentLoaded', function() {
     var links = document.querySelectorAll('.cv-navigation a');
     var dropbtn = document.getElementById('socials');
     var dropdownContent = document.querySelector('.dropdown-content');
