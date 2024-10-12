bc29e8a435106ef846c0d9e1d969c9645f9ac104
Sergey Babenko <sergj.babenko@gmail.com>
2024-10-12 10:28:48 +0300

    feat: lab4

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     var links = document.querySelectorAll('.cv-navigation a');
-    links.forEach(function(link) {
+    var dropbtn = document.getElementById('socials');
+    var dropdownContent = document.querySelector('.dropdown-content');
+
+    links.forEach(function(link, index) {
         link.addEventListener('click', function(event) {
             links.forEach(function(link) {
                 link.classList.remove('active');
             });
             event.currentTarget.classList.add('active');
         });
+    });
+
+    dropbtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        dropdownContent.style.display = dropdownContent.style.display === 'grid' ? 'none' : 'grid';
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!dropbtn.contains(event.target) && !dropdownContent.contains(event.target)) {
+            dropdownContent.style.display = 'none';
+        }
     });
 });
